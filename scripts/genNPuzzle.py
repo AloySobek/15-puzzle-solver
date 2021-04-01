0b0e83b71185c5db81cfaea37f8758e10aa24cb7
Mikhail Pasechnik <michail.goodchild@gmail.com>
2021-04-02 01:30:18 +0300

    Update genNPuzzle.py

--- scripts/genNPuzzle.py
+++ scripts/genNPuzzle.py
@@ -2,34 +2,31 @@ from __future__ import print_function
 import random
 import sys
 
-def genNPuzzle(dim, lim):
+def genNPuzzle(dim):
     assert dim >= 0
-    assert lim >= 0
     print(dim)
-    for _ in range(0, dim):
-        for n in range(0, dim):
-            print(random.randrange(0, lim), end=' ' * random.randrange(1, 4))
+    a = list(range(0, dim * dim))
+    random.shuffle(a)
+    for i in range(0, dim):
+        for j in range(0, dim):
+            print(a[(i*dim) + j], end=' ' * random.randrange(1, 4))
         print('\n', end='')
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] in ('-h', '--help'):
-        print('python genNPuzzle.py WIDTH(int) UPPERLIMIT(int)')
+        print('python genNPuzzle.py WIDTH(int)')
         sys.exit(0)
 
     for i in range(0, 3):
         if random.randrange(0, 2):
             print(["# Comment here\n", "# Comment\tthere\n", "# Comment over\n", "# Comment here\n"][random.randrange(0, 3)], end='')
     
-    d = random.randrange(0, 100)
-    l = 4294967295
+    d = random.randrange(0, 25)
+
     try:
         d = int(sys.argv[1], 10)
     except (ValueError, IndexError):
         pass
-    try:
-        l = int(sys.argv[2], 10)
-    except (ValueError, IndexError):
-        pass
         
-    genNPuzzle(d, l)
+    genNPuzzle(d)
