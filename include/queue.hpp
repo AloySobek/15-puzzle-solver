bc2fdc5439bff64db9f449101b0c182b2bebd70a
AloySobek <supernova.wandering.star@gmail.com>
2021-04-02 01:33:40 +0300

    AVL tree done

--- include/queue.hpp
+++ include/queue.hpp
@@ -2,16 +2,15 @@
  * File              : queue.hpp
  * Author            : Rustam Khafizov <super.rustamm@gmail.com>
  * Date              : 31.03.2021 00:50
- * Last Modified Date: 31.03.2021 23:19
+ * Last Modified Date: 02.04.2021 01:33
  * Last Modified By  : Rustam Khafizov <super.rustamm@gmail.com>
  */
 
 #ifndef QUEUE_HPP
 # define QUEUE_HPP
 
-# include <cstdint>
-#include <endian.h>
 # include <iostream>
+# include <cstdint>
 # include <string>
 
 template <class T1>
@@ -19,40 +18,49 @@ class QueueNode
 {
 public:
     QueueNode *parent{nullptr};
-    QueueNode *right{nullptr};
-    QueueNode *left{nullptr};
-    T1        key;
+    QueueNode *right {nullptr};
+    QueueNode *left  {nullptr};
+    
+    T1 *key;
 
-    QueueNode(T1 key) : key{key} {}
+    QueueNode(T1 *key) : key{key} {}
 };
-
 
 template <class T1>
 class PriorityQueue
 {
 public:
     PriorityQueue() {}
-    PriorityQueue(T1 first_element) : q_root{QueueNode<T1>(first_element)} {}
 
-    PriorityQueue<T1> *push(T1 i) { q_root = _push(q_root, i); return (this); }
-    QueueNode<T1>     *pop()      { return(_pop(q_root)); }
+    PriorityQueue<T1> *push(T1 *key)
+    {
+        if (key) q_root = _push(q_root, key);
+        return (this);
+    }
+    T1 *pop()
+    {
+        return(_pop(q_root)->key);
+    }
 
-    void tree_print() { _tree_print("", q_root, false); }
+    bool empty()           { return (q_root ? false : true); }
+    bool contains(T1 *key) { return _contains(q_root, key); }
+    void tree_print()      { _tree_print("", q_root, false); }
     
 private:
     QueueNode<T1> *q_root{nullptr};
+
+    int64_t _height(QueueNode<T1> *root);
 
     QueueNode<T1> *_left_rotation(QueueNode<T1> *root);
     QueueNode<T1> *_left_right_rotation(QueueNode<T1> *root);
     QueueNode<T1> *_right_rotation(QueueNode<T1> *root);
     QueueNode<T1> *_right_left_rotation(QueueNode<T1> *root);
 
-    QueueNode<T1> *_push(QueueNode<T1> *root, T1 item);
+    QueueNode<T1> *_balance(QueueNode<T1> *root);
+    QueueNode<T1> *_push(QueueNode<T1> *root, T1 *item);
     QueueNode<T1> *_pop(QueueNode<T1> *root);
 
-    int64_t        _height(QueueNode<T1> *root);
-    QueueNode<T1> *_balance(QueueNode<T1> *root);
-
+    bool _contains(QueueNode<T1> *root, T1 *key);
     void _tree_print(const std::string &prefix, QueueNode<T1> *root, bool left);
 };
 
@@ -126,17 +134,17 @@ QueueNode<T1> *PriorityQueue<T1>::_balance(QueueNode<T1> *root)
 }
 
 template<class T1>
-QueueNode<T1> *PriorityQueue<T1>::_push(QueueNode<T1> *root, T1 item)
+QueueNode<T1> *PriorityQueue<T1>::_push(QueueNode<T1> *root, T1 *item)
 {
     if (!root)
         return (new QueueNode<T1>(item));
-    else if (root->key > item)
+    else if (*(root->key) > *item)
     {
         root->left = _push(root->left, item);
         root->left->parent = root;
         root = _balance(root);
     }
-    else if (root->key <= item)
+    else if (*(root->key) <= *item)
     {
         root->right = _push(root->right, item);
         root->right->parent = root;
@@ -168,7 +176,26 @@ QueueNode<T1> *PriorityQueue<T1>::_pop(QueueNode<T1> *root)
             q_root = nullptr;
         ghost = root;
     }
+    else
+    {
+        if (root == q_root)
+            q_root = _balance(root);
+        else
+            _balance(root);
+    }
     return (ghost);
+}
+
+template<class T1>
+bool PriorityQueue<T1>::_contains(QueueNode<T1> *root, T1 *key)
+{
+    if (!root)
+        return false;
+    if (*(root->key) == *key)
+        return true;
+    if (*(root->key) > *key)
+        return (_contains(root->left, key));
+    return (_contains(root->right, key));
 }
 
 template<class T1>
@@ -177,7 +204,7 @@ void PriorityQueue<T1>::_tree_print
 {
     if (!root)
         return ;
-    std::cout << prefix << (left ? "├──" : "└──" ) << root->key << std::endl;
+    std::cout << prefix << (left ? "├──" : "└──" ) << *(root->key) << std::endl;
     _tree_print(prefix + (left ? "│  " : "   "), root->left, true);
     _tree_print(prefix + (left ? "│  " : "   "), root->right, false);
 }
