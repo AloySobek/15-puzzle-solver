145e557a1ea938190186f0853d57f47ca6d3782a
rustam <supernova.wandering.star@gmail.com>
2021-04-05 18:39:14 +0300

    Manhattan heuristic added

--- include/heuristics.hpp
+++ include/heuristics.hpp
@@ -2,7 +2,7 @@
  * File              : heuristics.hpp
  * Author            : Rustam Khafizov <super.rustamm@gmail.com>
  * Date              : 03.04.2021 16:18
- * Last Modified Date: 03.04.2021 17:40
+ * Last Modified Date: 05.04.2021 18:35
  * Last Modified By  : Rustam Khafizov <super.rustamm@gmail.com>
  */
 
@@ -12,6 +12,7 @@
 # include <cstdint>
 # include "state.hpp"
 
-int64_t hamming(State *f_state, State *state);
+int64_t hamming(State *final, State *intermediate);
+int64_t manhattan(State *final, State *intermediate);
 
 #endif
